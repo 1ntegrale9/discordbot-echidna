356205b63eefd744ec315afdbb356997d3b04cf9
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2018-07-02 03:12:53 +0900

    Refactor

--- discordbot.py
+++ discordbot.py
@@ -25,6 +25,39 @@ async def delete_role(message):
     else:
         return '役職 {} は存在しません'.format(arg)
 
+def toggle_debug_mode(mode):
+    global debug_mode
+    debug_mode = mode
+    return 'デバッグモードを{}にしました'.format('ON' if mode else 'OFF')
+
+async def set_roles(message):
+    add, rm, pd, nt = [], [], [], []
+    role_names = [role.name.lower() for role in message.server.roles]
+    for role_name in message.content.split()[1:]:
+        if role_name.lower() in role_names:
+            index = role_names.index(role_name.lower())
+            role = message.server.roles[index]
+            if role in message.author.roles:
+                rm.append(role)
+            elif role.permissions.administrator:
+                pd.append(role)
+            else:
+                add.append(role)
+        else:
+            nt.append(role_name)
+    msg = ''
+    if add:
+        await client.add_roles(message.author, *add)
+        msg = msg + '\n役職 {} を付与しました'.format(', '.join([r.name for r in add]))
+    if rm:
+        await client.remove_roles(message.author, *rm)
+        msg = msg + '\n役職 {} を解除しました'.format(', '.join([r.name for r in rm]))
+    if pd:
+        msg = msg + '\n役職 {} は追加できません'.format(', '.join([r.name for r in pd]))
+    if nt:
+        msg = msg + '\n役職 {} は存在しません'.format(', '.join(nt))
+    return msg
+
 @client.event
 async def on_ready():
     print('Logged in')
@@ -32,42 +65,16 @@ async def on_ready():
 @client.event
 async def on_message(message):
     try:
-        global debug_mode
         if message.author == client.user:
             return
         else:
             msg = ''
             remark = message.content
-            if remark.startswith('/role'):
-                if remark.startswith('/role '):
-                    add, rm, pd, nt = [], [], [], []
-                    role_names = [role.name.lower() for role in message.server.roles]
-                    for role_name in remark.split()[1:]:
-                        if role_name.lower() in role_names:
-                            index = role_names.index(role_name.lower())
-                            role = message.server.roles[index]
-                            if role in message.author.roles:
-                                rm.append(role)
-                            elif role.permissions.administrator:
-                                pd.append(role)
-                            else:
-                                add.append(role)
-                        else:
-                            nt.append(role_name)
-                    msg = ''
-                    if add:
-                        await client.add_roles(message.author, *add)
-                        msg = msg + '\n役職 {} を付与しました'.format(', '.join([r.name for r in add]))
-                    if rm:
-                        await client.remove_roles(message.author, *rm)
-                        msg = msg + '\n役職 {} を解除しました'.format(', '.join([r.name for r in rm]))
-                    if pd:
-                        msg = msg + '\n役職 {} は追加できません'.format(', '.join([r.name for r in pd]))
-                    if nt:
-                        msg = msg + '\n役職 {} は存在しません'.format(', '.join(nt))
-                else:
-                    role_names = [role.name for role in message.server.roles if not (role.is_everyone or role.managed or role.permissions.administrator)]
-                    msg = 'このサーバーにある役職は以下の通りです\n' + ', '.join(role_names) if role_names else '役職がありません'
+            if remark == '/role':
+                role_names = [role.name for role in message.server.roles if not (role.is_everyone or role.managed or role.permissions.administrator)]
+                msg = 'このサーバーにある役職は以下の通りです\n' + ', '.join(role_names) if role_names else '役職がありません'
+            if remark.startswith('/role '):
+                msg = await set_roles(message)
             if remark == '/role_self':
                 role_names = [role.name[1:] for role in message.author.roles if not role.is_everyone]
                 msg = ', '.join(role_names) if role_names else '役職が設定されていません'
@@ -76,18 +83,17 @@ async def on_message(message):
             if remark.startswith('/delete_role '):
                 msg = await requires_admin(message, delete_role)
             if remark == '/debug_on':
-                debug_mode = True
-                msg = 'デバッグモードをONにしました'
+                msg = toggle_debug_mode(True)
             if remark == '/debug_off':
-                debug_mode = False
-                msg = 'デバッグモードをOFFにしました'
+                msg = toggle_debug_mode(False)
             if msg:
                 mention = str(message.author.mention) + ' '
                 await client.send_message(message.channel, mention + msg)
     except Exception as e:
         await client.send_message(message.channel, str(e))
         if debug_mode:
-            await client.send_message(message.channel, '```\n{}\n```'.format(traceback.format_exc()))
+            traceback_msg = '```\n{}\n```'.format(traceback.format_exc())
+            await client.send_message(message.channel, traceback_msg)
     else:
         pass
     finally:
