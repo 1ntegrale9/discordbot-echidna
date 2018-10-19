c2654db37eef7daa58a35d2d0f20d2545041da3e
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2018-10-20 03:02:08 +0900

    Remove: リアクション通知機能

--- discordbot.py
+++ discordbot.py
@@ -12,7 +12,6 @@ import redis
 
 client = Client()
 r = redis.from_url(os.environ['REDIS_URL'], decode_responses=True)
-DEVELOPER = discord.User(id='314387921757143040')
 scrapbox_api_url = 'https://scrapbox.io/api/pages/'
 
 
@@ -61,22 +60,6 @@ async def on_message(message: Message) -> None:
                 '\n',
                 ])
             print(log)
-
-
-@client.event
-async def on_reaction_add(reaction: Reaction, user: User) -> None:
-    """リアクションが付いた時に実行する"""
-    if reaction.message.author == DEVELOPER:
-        msg = f'{user} が {reaction.message.content} に {reaction.emoji} を付けました'
-        await client.send_message(DEVELOPER, msg)
-
-
-@client.event
-async def on_reaction_remove(reaction: Reaction, user: User) -> None:
-    """リアクション削除時に実行する"""
-    if reaction.message.author == DEVELOPER:
-        msg = f'{user} が {reaction.message.content} の {reaction.emoji} を削除しました'
-        await client.send_message(DEVELOPER, msg)
 
 
 def main() -> None:
