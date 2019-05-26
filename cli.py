e8d1c29fc2439a1d61a93ed3dd47877998ef8f81
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2019-05-26 16:30:29 +0900

    Update: cli

--- cli.py
+++ cli.py
@@ -5,7 +5,14 @@ client = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
+def is_developer():
+    async def predicate(ctx):
+        return ctx.author.id == ID.user.developer
+    return commands.check(predicate)
+
+
 @client.command()
+@is_developer()
 async def do(ctx):
     for channel in ctx.guild.channels:
         payload = {
