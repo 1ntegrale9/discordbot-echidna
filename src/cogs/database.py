d8effccb345762a67f2e8701b1800f2a9a018821
1ntegrale9 <1ntegrale9uation@gmail.com>
2020-04-25 04:33:27 +0900

    refactor

---
+++ src/cogs/database.py
@@ -0,0 +1,85 @@
+import os
+import typing
+import redis
+from discord.ext import commands
+
+
+class Database(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.redis = redis.from_url(
+            os.environ['REDIS_URL'],
+            decode_responses=True,
+        )
+
+    @commands.group()
+    async def db(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('オプションを指定してください。')
+
+    @db.command()
+    async def get(self, ctx, keyword: str):
+        key = f'{ctx.guild.id}:{keyword}'
+        if not self.redis.exists(key):
+            return await ctx.send(f'key:{keyword} は存在しません。')
+        await ctx.send(normalize(self.redis.smembers(key)))
+
+    @db.command(aliases=['set'])
+    async def _set(self, ctx, keyword: str, related: str):
+        key = f'{ctx.guild.id}:{keyword}'
+        self.redis.sadd(key, related)
+        self.redis.sadd(ctx.guild.id, keyword)
+        await ctx.send(f'{keyword} に {related} を追加しました。')
+
+    @db.command()
+    async def delete(self, ctx, keyword: str, related: typing.Optional[str]):
+        key = f'{ctx.guild.id}:{keyword}'
+        if not self.redis.exists(key):
+            return await ctx.send(f'key:{keyword} は存在しません。')
+        if related:
+            self.redis.srem(key, related)
+            await ctx.send(f'key:{keyword} から {related} を削除しました。')
+        else:
+            self.redis.delete(key)
+            self.redis.srem(ctx.guild.id, key)
+            await ctx.send(f'key:{keyword} を削除しました。')
+
+    @db.command(aliases=['list'])
+    async def _list(self, ctx):
+        if not self.redis.exists(ctx.guild.id):
+            return await ctx.send('データが存在しません。')
+        await ctx.send(normalize(self.redis.smembers(ctx.guild.id)))
+
+    @db.command()
+    @commands.is_owner()
+    async def keys(self, ctx):
+        await ctx.send(' '.join(sorted(self.redis.keys())))
+
+    @db.command()
+    @commands.is_owner()
+    async def flushdb(self, ctx):
+        self.redis.flushdb()
+        await ctx.send('Deleted all keys in the current database.')
+
+    @commands.command(aliases=['教えて'])
+    async def oshiete(self, ctx, keyword: str):
+        key = f'{ctx.guild.id}:{keyword}'
+        if not self.redis.exists(key):
+            await ctx.send('？')
+        await ctx.send(f'{keyword} は {self.redis.get(key)}')
+
+    @commands.command(aliases=['覚えて'])
+    async def oboete(self, ctx, keyword: str, related: str):
+        _id = ctx.guild.id
+        key = f'{_id}:{keyword}'
+        self.redis.set(key, related)
+        self.redis.sadd(_id, keyword)
+        await ctx.send(f'{keyword} は {related}、覚えました！')
+
+
+def normalize(data):
+    return '\n'.join(sorted(data))
+
+
+def setup(bot):
+    bot.add_cog(Database(bot))
--- src/discordbot.py
+++ src/discordbot.py
@@ -1,215 +1,26 @@
-import discord
-from discord import Embed
-from discord.ext import commands
-import logging
 import os
-import traceback
-import re
-from datetime import datetime
-from db import knowledge
-from db import command_db
-from random import randint
-from utils import generate_random_color
-from utils import generate_random_token
-from utils import grouping
-from info import get_help
-from config import get_id
-from echidna.daug import get_default_embed
+import logging
+from operator import mul
+from functools import reduce
+from discord.ext import commands
+from datetime.datetime import now
 
-id = 462810739204161537
 logging.basicConfig(level=logging.INFO)
-bot = commands.Bot(command_prefix=('/', f'<@{id}> ', f'<@!{id}> '), help_command=None)
-token = os.environ['DISCORD_BOT_TOKEN']
-
-ID = get_id()
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('/'), help_command=None)
 
 
 @bot.event
 async def on_ready():
-    channel_login = bot.get_channel(id=ID.channel.login)
-    await channel_login.send(str(datetime.now()))
-
-
-@bot.event
-async def on_message(message):
-    try:
-        if message.author.bot:
-            return
-        if not isinstance(message.channel, discord.channel.TextChannel):
-            return
-        await parse(message)
-        await bot.process_commands(message)
-    except Exception as e:
-        await message.channel.send(str(e))
-        channel_traceback = bot.get_channel(id=ID.channel.traceback)
-        message_traceback = f'```\n{traceback.format_exc()}\n```'
-        await channel_traceback.send(message_traceback)
-
-
-def is_developer():
-    async def predicate(ctx):
-        return ctx.author.id == ID.user.developer
-    return commands.check(predicate)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-@bot.command()
-async def neko(ctx):
-    await ctx.send('にゃーん')
-
-
-@bot.command()
-@is_developer()
-async def guilds(ctx):
-    for s in bot.guilds:
-        is_admin = s.me.guild_permissions.administrator
-        await ctx.channel.send(f'{s.name}：{is_admin}')
-
-
-@bot.command()
-@is_developer()
-async def purge(ctx):
-    while (await ctx.message.channel.purge()):
-        pass
-
-
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def topic(ctx, topic):
-    await ctx.channel.edit(topic=topic)
-
-
-@bot.command()
-async def member_status(ctx):
-    text = ctx.author.voice.voice_channel.name
-    await ctx.send(text)
-
-
-@bot.command()
-async def member(ctx):
-    arg = ctx.guild.member_count
-    text = f'このサーバーには{arg}人のメンバーがいます'
-    await ctx.send(text)
-
-
-@bot.command()
-@is_developer()
-async def debug_role(ctx):
-    embed = Embed(title="role name", description="role id")
-    for role in ctx.guild.roles:
-        embed.add_field(name=role.name, value=role.id, inline=False)
-    await ctx.send(embed=embed)
-
-
-@bot.command()
-@is_developer()
-async def debug_guild(ctx):
-    await ctx.send(ctx.guild.id)
-
-
-@bot.command()
-async def help(ctx):
-    embed = get_help(bot)
-    await ctx.send(embed=embed)
-
-
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def create_role(ctx, name: str):
-    if name.lower() in [role.name.lower() for role in ctx.guild.roles]:
-        msg = 'その役職は既に存在します'
-    else:
-        await ctx.guild.create_role(name=name)
-        msg = f'役職 {name} を作成しました'
-    await ctx.send(msg)
-
-
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def delete_role(ctx, arg):
-    role_names = [role.name.lower() for role in ctx.guild.roles]
-    if arg in role_names:
-        index = role_names.index(arg)
-        role = ctx.guild.roles[index]
-        await role.delete()
-        msg = f'役職 {role.name} を削除しました'
-    else:
-        msg = f'役職 {arg} は存在しません'
-    await ctx.send(msg)
-
-
-@bot.command()
-async def randcolor(ctx):
-    await ctx.send(str(generate_random_color()))
-
-
-@bot.command()
-async def randid(ctx):
-    await ctx.channel.send(randint(10 ** 17, 10 ** 18 - 1))
-
-
-@bot.command()
-async def randtoken(ctx):
-    await ctx.channel.send(generate_random_token())
-
-
-@bot.command()
-async def db(ctx, *args):
-    msg = await command_db(ctx.message, bot)
-    await ctx.send(msg)
-
-
-async def parse(message):
-    if re.fullmatch('/[0-9]+', message.content):
-        number = int(message.content[1:])
-        msg = await grouping(message, number)
-        await message.channel.send(msg)
-    if message.content.startswith('/echo '):
-        if message.author.id == ID.user.developer:
-            arg = message.content.split('/echo ')[1]
-            await message.delete()
-            await message.channel.send(arg)
-        else:
-            msg = 'コマンドを実行する権限がありません'
-            await message.channel.send(msg)
-    if message.content:
-        if str(bot.user.id) in message.content.split()[0]:
-            msg = knowledge(message)
-            await message.channel.send(msg)
-    if message.content.startswith('echo:'):
-        await echo(message)
-    if message.content.startswith('embed:'):
-        await embed(message)
-
-
-async def send2developer(msg):
-    developer = bot.get_user(ID.user.developer)
-    dm = await developer.create_dm()
-    await dm.send(msg)
-
-
-async def echo(message):
-    await message.delete()
-    n = len('echo:')
-    text = message.content[n:]
-    await message.channel.send(text)
-
-
-async def embed(message):
-    if message.author.guild_permissions.administrator:
-        await message.delete()
-        n = len('embed:')
-        embed = get_default_embed(message.content[n:])
-        await message.channel.send(embed=embed)
+    ID = reduce(mul, (2, 7, 11, 33637, 223253, 434803))
+    await bot.get_channel(ID).send(now().strftime("%Y/%m/%d %H:%M:%S"))
 
 
 if __name__ == '__main__':
     bot.load_extension('jishaku')
     bot.load_extension('dispander')
     bot.load_extension('discordbotjp.cog')
+    bot.load_extension('cogs.admin')
+    bot.load_extension('cogs.database')
+    bot.load_extension('cogs.public')
     bot.load_extension('cogs.werewolf')
-    bot.run(token)
+    bot.run(os.environ['DISCORD_BOT_TOKEN'])
