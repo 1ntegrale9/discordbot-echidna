ec6f79774343a509338880621459748dc254cc33
NoName <sevenc-nanashi@sevenbot.jp>
2021-02-23 18:40:25 +0900

    コマンドの脆弱性を修正
    https://imgur.com/8Fz42wa
    このようにeveryone出来てしまう問題を修正しました。

--- src/discordbot.py
+++ src/discordbot.py
@@ -11,6 +11,7 @@ bot = commands.Bot(
     command_prefix=commands.when_mentioned_or('/'),
     help_command=None,
     intents=discord.Intents.all(),
+    allowed_mentions=discord.AllowedMentions.none(),
 )
 config = {
     'Daug': {
